0dbf7caf54aa5ef6a52d77e2581243cf80668111
Daniel Craig <daniel@wearebeef.co.uk>
2010-02-24 17:29:58 +0000

    get slide show slides if passed a slide show permalink

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,8 +1,15 @@
 class SlidesController < ApplicationController
 
   def index
+    if params[:slide_show_id].blank?
+      @slides = Slide.published
+    else
+      @slideshow = SlideShow.find_by_permalink(params[:slide_show_id])
+      @slides = @slideshow.slides.published
+    end
+    
     respond_to do |format|
-      format.json  { render :json => Slide.published }
+      format.json  { render :json => @slides }
     end
   end
 
